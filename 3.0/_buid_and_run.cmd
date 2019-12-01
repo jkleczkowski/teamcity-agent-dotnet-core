05a57f240d69facca435b6d61d2233c24fdaf4a8
Jacek Kleczkowski <jkleczkowski@gmail.com>
2019-12-01 18:15:53 +0100

    [tc] docker in docker

---
+++ 3.0/_buid_and_run.cmd
@@ -0,0 +1,8 @@
+docker build -t jkleczkowski/teamcity-agent-dotnet-core .
+docker rm tca --force
+
+@rem -e SERVER_URL=https://teamcity.ksoft.biz 
+
+@rem 
+docker run --rm --name tca -v c:/DockerBuildAgent/conf:/data/teamcity_agent/conf -v c:/DockerBuildAgent/work:/opt/buildagent/work jkleczkowski/teamcity-agent-dotnet-core
+
