212e57bdd5348e40c1684496d8fb7f23f7733a42
Jacek Kleczkowski <jkleczkowski@gmail.com>
2019-12-18 00:20:17 +0100

    [TCA] layers optimalization and two versions split

--- latest/_build_and_run.cmd
+++ latest/_build_and_run.cmd
@@ -1,7 +1,6 @@
-docker build -t jkleczkowski/teamcity-agent-dotnet-core:next .
+docker build -t jkleczkowski/teamcity-agent-dotnet-core:latest .
+docker build -t jkleczkowski/teamcity-agent-dotnet-core:full --build-args CORE_VERSIONS="dotnet-sdk-2.1 dotnet-sdk-2.2 dotnet-sdk-3.0 dotnet-sdk-3.1" .
 
-@rem 
-docker rm tca --force
+@rem docker rm tca --force
 
-@rem 
-docker run --rm --name tca -v c:/DockerBuildAgent/conf:/data/teamcity_agent/conf -v c:/DockerBuildAgent/work:/opt/buildagent/work jkleczkowski/teamcity-agent-dotnet-core:next
+@rem docker run --rm --name tca -v c:/DockerBuildAgent/conf:/data/teamcity_agent/conf -v c:/DockerBuildAgent/work:/opt/buildagent/work jkleczkowski/teamcity-agent-dotnet-core:full
