6e08593cb0beb72cb7d89af897dba48d08f529c7
Jacek Kleczkowski <jkleczkowski@gmail.com>
2019-12-21 22:24:33 +0100

    [TCA] added maven + fix for agent script

--- latest/_build_and_run.cmd
+++ latest/_build_and_run.cmd
@@ -1,6 +1,10 @@
 docker build -t jkleczkowski/teamcity-agent-dotnet-core:latest .
-docker build -t jkleczkowski/teamcity-agent-dotnet-core:full --build-args CORE_VERSIONS="dotnet-sdk-2.1 dotnet-sdk-2.2 dotnet-sdk-3.0 dotnet-sdk-3.1" .
+@rem docker build -t jkleczkowski/teamcity-agent-dotnet-core:full --build-args CORE_VERSIONS="dotnet-sdk-2.1 dotnet-sdk-2.2 dotnet-sdk-3.0 dotnet-sdk-3.1" .
 
 @rem docker rm tca --force
 
 @rem docker run --rm --name tca -v c:/DockerBuildAgent/conf:/data/teamcity_agent/conf -v c:/DockerBuildAgent/work:/opt/buildagent/work jkleczkowski/teamcity-agent-dotnet-core:full
+
+@rem docker tag jkleczkowski/teamcity-agent-dotnet-core:next jkleczkowski/teamcity-agent-dotnet-core:latest
+@rem 
+docker push jkleczkowski/teamcity-agent-dotnet-core:latest
